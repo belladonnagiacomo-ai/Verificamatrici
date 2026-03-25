113eaf20d838a2637ddc9c006cfa59060a12f279
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-03-25 12:54:07 +0100

    mod

--- Verificamatrici/Program.cs
+++ Verificamatrici/Program.cs
@@ -6,32 +6,64 @@
         {
             int[,] m2 = new int[m.GetLength(0), m.GetLength(1)];
             bool verifica = false;
+            int k = 0, r = 0; 
             for(int i = 0; i < m.GetLength(0); i++)
             {
+                k = 0;
                 for(int j = 0;  j < m.GetLength(1); j++)
                 {
-                    if(m[i, j]  == m[i + 1, j])
+                    if(m[r, j]  == m[i + r, j])
                     {
-                        verifica = true;
-                        return verifica;
+                        k++;
                     }
+
                    
+                }
+                r++;
+                if(k == 4)
+                {
+                    verifica = true;
+                    return verifica;
                 }
             }
             return verifica;
             
         }
+        static void cerca1(int[,] m, int colonna)
+        {
+            int c = 0;
+            for(int i = 0; i < m.GetLength(0); i++)
+            {
+                for(int j = 0; j < m.GetLength(1); j++)
+                {
+                    if (m[i,j] == 1)
+                    {
+                        c = j;
+                    }
+                }
+            }
+            if(colonna == c)
+            {
+                Console.WriteLine("Hai indovinato la posizione");
+            }
+            else if(colonna > c)
+            {
+                Console.WriteLine("Alto");
+            }
+            else if(colonna < c)
+            {
+                Console.WriteLine("Basso");
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            int[,] m = { {0,1,1,1}, { 1,1,0,0}, { 1,1,1,1}, { 0,0,1,1} };
-            if(RigheU(m) == true)
-            {
-                Console.WriteLine("c'è una coppia di righe uguali");
-            }
-            else
-            {
-                Console.WriteLine("non ci sono righe uguali");
-            }
+            int[,] m = { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+            Console.WriteLine("Dammi la colonna");
+            int colonna = Convert.ToInt32(Console.ReadLine());
+            cerca1(m, colonna);
+           
         }
     }
 }
